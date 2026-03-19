a00603136c5ef90af8072a18b4d468f85ea10938
WonJo-Jung <unprecedentedgame222@gmail.com>
2026-03-20 05:38:23 +0900

    add Analytics

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import Head from "next/head";
 
@@ -25,6 +26,7 @@ export default function Home() {
         <a href="/support"><br></br><br></br>Support</a>
 
         <SpeedInsights />
+        <Analytics />
       </main>
     </>
   );
